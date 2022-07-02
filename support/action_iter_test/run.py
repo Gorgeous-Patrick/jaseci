51beda30046835783b0a440d7d159f3030dc9d5a
Patrick Li <1543162420@qq.com>
2022-07-02 13:17:38 -0400

    edited config for action iter tests

--- support/action_iter_test/run.py
+++ support/action_iter_test/run.py
@@ -16,10 +16,8 @@ jaseci_path = "/jaseci/jaseci_kit/jaseci_kit/modules"
 # def getRunCommand(walkerName: str):
 #     return f"jsctl -f session walker run {walkerName}"
 
-
 def localActionPath(kit_module: str):
     return os.path.join(jaseci_path, kit_module)
-
 
 # def getActionLoadCommand(module_path: str):
 #     return f"jsctl actions load local {module_path}"
@@ -29,7 +27,6 @@ def localActionPath(kit_module: str):
 
 HOST = "http://clarity3.eecs.umich.edu:30001"
 auth_header = {}
-
 
 def login():
     userName = "jaclang0@jaseci.org"
@@ -43,7 +40,6 @@ def login():
     print(auth_header)
     return auth_header
 
-
 def load_module_actions(abs_path: str):
     response = requests.post(
         HOST + "/js_admin/actions_load_module",
@@ -51,8 +47,6 @@ def load_module_actions(abs_path: str):
         json={"mod": abs_path},
     )
     print(f"Load Actions (module): {response.text}")
-
-
 def load_actions(abs_path: str):
     response = requests.post(
         HOST + "/js_admin/actions_load_local",
@@ -60,7 +54,6 @@ def load_actions(abs_path: str):
         json={"file": abs_path},
     )
     print(f"Load Actions (local): {response.text}")
-
 
 def getSentinel(codePath: str):
     file = open(codePath, "r")
@@ -77,29 +70,56 @@ def getSentinel(codePath: str):
     )
     print(f"Sentinel: f{response.text}")
     snt = response.json()[0]["jid"]
-    response = requests.post(
-        HOST + "/js/graph_create", headers=auth_header, json={"set_active": True}
-    )
+    response =requests.post(
+            HOST + "/js/graph_create", headers=auth_header, json={"set_active": True}
+        )
     print(f"Create Graph: f{response.text}")
     return snt
 
-
-def walkerRun(SNT, req):
-    response = requests.post(HOST + "/js/walker_run", headers=auth_header, json=req)
-    walkerName = req["name"]
+def walkerRun(walkerName: str, SNT):
+    req = {"name": walkerName, "snt": SNT}
+    response = requests.post(
+            HOST + "/js/walker_run",
+            headers = auth_header,
+            json = req
+            )
     print(f"Walker Run ({walkerName}): f{response.text}")
-
-
+file = open("config.json", "r")
+conf = json.load(file)
 login()
-snt = getSentinel("zsb/zsb.jac")
-json = {
-    "name": "add_bot",
-    "nd": "active:graph",
-    "ctx": {
-        "name": "locustBot",
-        "description": "this is a test bot for Locust testing",
-    },
-    "snt": "active:sentinel",
-}
-
-walkerRun(snt, json)
+output = open("time.csv", "w")
+writer = csv.writer(output, delimiter=",")
+writer.writerow(["Module Name", "Action Name", "iteration Number", "Duration", "Local/Remote"])
+# os.system(initCommand())
+for module in conf:
+    name = module["kit_module"]
+    module_name = module.get("module_name", name)
+    # if module_name != "use_qa":
+    #     continue
+    local_path = module["local_path"]
+    load_module = module.get("load_module", False)
+    abs_action_path = localActionPath(local_path)
+    if load_module:
+        load_module_actions(f"jaseci_kit.{module_name}")
+    else:
+        load_actions(abs_action_path)
+#     load_action_cmd = getActionLoadCommand(abs_action_path)
+#     print(load_action_cmd)
+#     os.system(load_action_cmd)
+    actions = module["actions"]
+    for action in actions:
+        action_name = action["name"]
+        iteration = action["iter"]
+        snt = getSentinel(f"{module_name}/{action_name}.jac")
+#         cmd = getSntCommand(f"{name}/{action_name}.jac")
+#         print(cmd)
+#         os.system(cmd)
+#         cmd = getRunCommand(f"{action_name}")
+        t = time.time()
+        walkerRun(action_name, snt)
+        duration = time.time() - t
+        print(duration)
+        writer.writerow([module_name, action_name, iteration, duration] + ["local"])
+#         print(cmd)
+#         os.system(cmd)
+output.close()
