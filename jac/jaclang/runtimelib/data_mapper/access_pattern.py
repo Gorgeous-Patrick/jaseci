ccfce1c432078ae7a85414f75dc460caac86e38f
Gorgeous Patrick <baichuanli@yahoo.com>
2025-07-11 14:41:04 -0400

    feat: conditional visit (not tested)

--- jac/jaclang/runtimelib/data_mapper/access_pattern.py
+++ jac/jaclang/runtimelib/data_mapper/access_pattern.py
@@ -2,29 +2,19 @@
 
 from dataclasses import dataclass
 from typing import TypeAlias
-from jaclang.runtimelib.data_mapper.visit_restrictions import Block, VisitRestriction
-import networkx as nx
-import random
 
-from .static_phase import VisitInfo
+import jaclang.compiler.unitree as uni
+
+import networkx as nx
+
+from .visit_restrictions import VisitRestriction, get_visit_restriction_of_single_visit
 
 
 def filter_neighbors(
-    node_idx: int, network: nx.DiGraph, visit_info: list[VisitInfo], walker_type: str
+    node_idx: int, network: nx.DiGraph, visit_restrictions: VisitRestriction
 ) -> list[int]:
     """Filter neighbors based on visit info and walker type."""
     filtered_neighbors = []
-
-    # Get the node type of the current node
-    current_node_type = network.nodes[node_idx].get("node_type")
-
-    # Find matching visit info for this walker type and node type
-    matching_visits = [
-        visit
-        for visit in visit_info
-        if visit.walker_type == walker_type
-        and visit.from_node_type == current_node_type
-    ]
 
     # Get all neighbors
     for neighbor_idx in network.neighbors(node_idx):
@@ -34,59 +24,61 @@ def filter_neighbors(
             continue
         edge_type = edge_data.get("edge_type")
 
-        # Check if any visit info matches the edge type
-        for visit in matching_visits:
-            # If no specific edge type is required or edge type matches
-            if visit.edge_type is None or visit.edge_type == edge_type:
-                filtered_neighbors.append(neighbor_idx)
-                break  # Found a match, no need to check other visits
+        # If no specific edge type is required or edge type matches
+        if (
+            visit_restrictions.edge_type is None
+            or visit_restrictions.edge_type == edge_type
+        ):
+            filtered_neighbors.append(neighbor_idx)
+            break  # Found a match, no need to check other visits
 
     return filtered_neighbors
+
 
 SymbolicVisit: TypeAlias = tuple[int, list[int]]
 SymbolicVisits: TypeAlias = list[SymbolicVisit]
 SymbolicVisitPossibilities: TypeAlias = list[SymbolicVisits]
 
-def get_access_pattern_single_walker_one_run(start_idx, network: nx.DiGraph, flow: Block, possibilities: SymbolicVisitPossibilities) -> SymbolicVisitPossibilities:
-    res = possibilities.copy()
-    for step in flow:
-        if isinstance(step, VisitRestriction):
-            # If the step is a VisitRestriction, find neighbors matching the edge_type
-            neighbors = []
-            for neighbor_idx in network.neighbors(start_idx):
-                edge_data = network.get_edge_data(start_idx, neighbor_idx)
-                if edge_data is None:
-                    continue
-                edge_type = edge_data.get("edge_type")
-                if step.edge_type is None or step.edge_type == edge_type:
-                    neighbors.append(neighbor_idx)
-            sym_visit = (step.index, neighbors)
-            res = [visits + [sym_visit] for visits in res]
 
-        # If it is a union type (i.e., tuple of Blocks), recursively process both branches and concatenate results
-        elif isinstance(step, tuple) and len(step) == 2:
-            left = get_access_pattern_single_walker_one_run(start_idx, network, step[0], res)
-            right = get_access_pattern_single_walker_one_run(start_idx, network, step[1], res)
-            res = left + right
-        
-    # Remove duplicates in res
-    # Each element in res is a list of SymbolicVisit tuples; remove duplicate lists
-    # We'll use tuple(map(tuple, ...)) to make lists hashable for set
-    unique_res = []
-    seen = set()
-    for visits in res:
-        key = tuple(visits)
-        if key not in seen:
-            unique_res.append(visits)
-            seen.add(key)
-    res = unique_res
-    return res
+def get_access_pattern_single_walker_one_run(
+    start_idx: int,
+    network: nx.DiGraph,
+    start_node: uni.UniCFGNode,
+    possibilities: SymbolicVisitPossibilities,
+) -> SymbolicVisitPossibilities:
+    """Get all the possible combinations of visit statements in one run."""
+    res = possibilities.copy()
+    bb_tail = start_node.get_tail()
+    node = start_node
+    while not (node is bb_tail):
+        if isinstance(node, uni.VisitStmt):
+            visit_restriction = get_visit_restriction_of_single_visit(node)
+            neighbors = filter_neighbors(start_idx, network, visit_restriction)
+            sym_visit = (visit_restriction.index, neighbors)
+            res = [visits + [sym_visit] for visits in res]
+        node = node.bb_out[0]
+
+    if len(node.bb_out) == 0:
+        return res
+    accumulated: SymbolicVisitPossibilities = []
+    for next_node in node.bb_out:
+        accumulated.extend(
+            get_access_pattern_single_walker_one_run(start_idx, network, next_node, res)
+        )
+
+    return accumulated
+
 
 @dataclass
 class SymbolicWalkerState:
+    """Simplified walker state."""
+
     container: list[int]
     loc: int
-    def new_possible_state(self, possible_visit: SymbolicVisits) -> "SymbolicWalkerState":
+
+    def _new_possible_state(
+        self, possible_visit: SymbolicVisits
+    ) -> "SymbolicWalkerState":
         container = self.container.copy()
         loc = container.pop(0)
         for visit in possible_visit:
@@ -94,26 +86,43 @@ class SymbolicWalkerState:
             if insert_loc < -len(container):
                 insert_loc = 0
             elif insert_loc < 0:
-                insert_loc += len(container) + 1;
+                insert_loc += len(container) + 1
             container = container[:insert_loc] + visit[1] + container[insert_loc:]
-        return SymbolicWalkerState(container= container, loc = loc)
+        return SymbolicWalkerState(container=container, loc=loc)
+
+    def new_possible_states(
+        self, possible_visits: SymbolicVisitPossibilities
+    ) -> list["SymbolicWalkerState"]:
+        """Generate a list of new possible states after each run."""
+        return [
+            self._new_possible_state(possible_visit)
+            for possible_visit in possible_visits
+        ]
 
 
-    def new_possible_states(self, possible_visits: SymbolicVisitPossibilities) -> list["SymbolicWalkerState"]:
-        return [self.new_possible_state(possible_visit) for possible_visit in possible_visits]
-
-def get_next_possible_states(state: SymbolicWalkerState, flows: dict[str, Block], network: nx.DiGraph) -> list[SymbolicWalkerState]:
+def get_next_possible_states(
+    state: SymbolicWalkerState, flows: dict[str, uni.Ability], network: nx.DiGraph
+) -> list[SymbolicWalkerState]:
+    """Get the list of all possible next states."""
     flow = flows[network.nodes[state.loc]["node_type"]]
-    possible_visits = get_access_pattern_single_walker_one_run(state.loc, network, flow, [[]])
+    possible_visits = get_access_pattern_single_walker_one_run(
+        state.loc, network, flow, [[]]
+    )
     return state.new_possible_states(possible_visits)
 
 
-def get_access_pattern_single_walker(start_idx: int, network: nx.DiGraph, flows: dict[str, Block]):
-    possible_states: list[SymbolicWalkerState] = [SymbolicWalkerState(container=[], loc = start_idx)]
-    
-    for _ in range(100):
-        new_possible_states : list[SymbolicWalkerState] = [];
-        for possible_state in possible_states:
-            new_possible_states = new_possible_states + get_next_possible_states(possible_state, flows, network)
-            possible_states = new_possible_states
+def get_access_pattern_single_walker(
+    start_idx: int, network: nx.DiGraph, flows: dict[str, uni.Ability]
+) -> None:
+    """Iterate multiple times."""
+    possible_states: list[SymbolicWalkerState] = [
+        SymbolicWalkerState(container=[], loc=start_idx)
+    ]
 
+    for _ in range(100):
+        new_possible_states: list[SymbolicWalkerState] = []
+        for possible_state in possible_states:
+            new_possible_states = new_possible_states + get_next_possible_states(
+                possible_state, flows, network
+            )
+            possible_states = new_possible_states
